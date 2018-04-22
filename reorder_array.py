efa318202b3151b105ad5fbb762c013f8058a43f
corine <corineru@163.com>
2018-04-22 21:46:03 +0800

    按奇偶调整数组

---
+++ reorder_array.py
@@ -0,0 +1,15 @@
+# 2018-04-22
+# xinru
+# file: 重新调整数组，奇数放前半部分，偶数放后半部分，并保持稳定
+
+
+def reOrderArray(self, array):
+    # write code here
+    ordered_array = []
+    for i in array:
+        if i % 2 != 0:
+            ordered_array.append(i)
+    for i in array:
+        if i % 2 == 0:
+            ordered_array.append(i)
+    return ordered_array
