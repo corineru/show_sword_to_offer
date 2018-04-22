a104f3f72ea331f70a5a67490259b021109d49cc
corine <corineru@163.com>
2018-04-22 21:58:03 +0800

    替换空格

--- Replace_space.py
+++ Replace_space.py
@@ -5,10 +5,10 @@
 
 class solution:
     def replaceSpace(self, s):
-    s_new = []
-    for i in s:
-        if i == ' ':
-            s_new.append('%20')
-        else:
-            s_new.append(i)
-    return ''.join(s_new)
+        s_new = []
+        for i in s:
+            if i == ' ':
+                s_new.append('%20')
+            else:
+                s_new.append(i)
+        return ''.join(s_new)
