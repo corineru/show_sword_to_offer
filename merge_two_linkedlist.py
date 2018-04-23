766dde9756f33426d7b114e116b74424a44ab874
corine <corineru@163.com>
2018-04-23 21:09:44 +0800

    合并两个有序的链表

--- merge_two_linkedlist.py
+++ merge_two_linkedlist.py
@@ -3,10 +3,10 @@
 # file:  合并两个排序的链表
 
 # -*- coding:utf-8 -*-
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 class Solution:
     # 返回合并后列表
     def Merge(self, pHead1, pHead2):
