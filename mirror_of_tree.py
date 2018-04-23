22355700a4ef17c7798f4265388c8985b825128f
corine <corineru@163.com>
2018-04-23 21:10:05 +0800

    求树的镜像

---
+++ mirror_of_tree.py
@@ -0,0 +1,21 @@
+
+# 2018-04-23
+# xinru
+# file:  判断是否是数的子结构
+
+
+# -*- coding:utf-8 -*-
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    # 返回镜像树的根节点
+    def Mirror(self, root):
+        # write code here
+        if not root:
+            return None
+        root.left, root.right= self.Mirror(root.right), self.Mirror(root.left)
+        return root
